4a196fc04c9b98bd124f379861971b7c5a61c793
Christian Colon <cjcolon723@gmail.com>
2026-01-15 17:27:07 -0400

    Main parts written. Next main function will be completed

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
-from textnode import TextNode, TextType
+import os
 
-
+def copy_directory(src_path, dest_path):
+    if os.path.exists(src_path):
+        src_tree = os.listdir(src_path)
+        print(src_tree)
+    if os.path.exists(dest_path):
+        print(f"{dest_path} exists")
 def main():
     print("Hello World")
-
+    copy_directory("./static/", "./public/")
 
 if __name__ == "__main__":
     main()
