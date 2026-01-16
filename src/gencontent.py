a6beb71c66f2bc2ac622fd38a1ff2cbbc85f8ea3
Christian Colon <cjcolon723@gmail.com>
2026-01-15 20:46:36 -0400

    Updated to work with GitHub Pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,7 +1,19 @@
 import os 
+from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
-def generate_page(from_path, template_path, dest_path):
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
+
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     # Read in markdown
     with open(from_path, 'r') as f:
@@ -15,6 +27,8 @@ def generate_page(from_path, template_path, dest_path):
     # Replace title and content in template
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", content)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     # Write content to destination file 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as f:
--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
-import os
+import sys
 from copystatic import copy_files_recursive
-from gencontent import generate_page
+from gencontent import generate_pages_recursive
    
 
 def main():
-    copy_files_recursive("./static/", "./public/")
-    root_dir = "content"
-    out_dir = "public"
-    for current_path, dirs, files in os.walk(root_dir):
-        for filename in files:
-            if filename.lower().endswith(".md"):
-                full_path = os.path.join(current_path, filename)
-                out_path = full_path.replace(root_dir, out_dir, 1) 
-                out_path = out_path.rsplit(".",1)[0] + ".html"
-                generate_page(full_path, "template.html", out_path)
-    generate_page("content/index.md", "template.html", "public/index.html")
+    # 1. Determine basepath
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    # Filesystem paths
+    dir_path_content = "content"
+    template_path = "template.html"
+    dest_dir_path = "docs" 
+    static_path = "static"
+    copy_files_recursive(static_path, dest_dir_path)
+    generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath)
 
 if __name__ == "__main__":
     main()
